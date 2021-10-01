e49bd4c93a02d7d5c3581c5ea41a8e1aee2d3381
suragch <studymongolian@gmail.com>
2021-10-01 16:56:25 +0800

    no challenges in this chapter

--- 03-basic-data-structures-in-dart/projects/challenge/README.md
+++
@@ -1,3 +0,0 @@
-Answers to the challenges for this chapter.
-
-You'll find the code in `bin/challenge.dart`.
--- 03-basic-data-structures-in-dart/projects/challenge/bin/challenge.dart
+++
@@ -1,4 +0,0 @@
-// Copyright (c) 2021 Razeware LLC
-// For full license & permission details, see LICENSE.
-
-void main() {}
