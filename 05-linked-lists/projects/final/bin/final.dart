50ca1cd340f959826c3804c9c37796bb7716adc7
suragch <studymongolian@gmail.com>
2021-10-04 18:07:02 +0800

    finished to insertAfter

--- 05-linked-lists/projects/final/bin/final.dart
+++ 05-linked-lists/projects/final/bin/final.dart
@@ -1,4 +1,54 @@
 // Copyright (c) 2021 Razeware LLC
 // For full license & permission details, see LICENSE.
 
-void main() {}
+import 'linked_list.dart';
+
+void main() {
+  nodes();
+  push();
+  append();
+  insertAfter();
+}
+
+void nodes() {
+  final node1 = Node(value: 1);
+  final node2 = Node(value: 2);
+  final node3 = Node(value: 3);
+
+  node1.next = node2;
+  node2.next = node3;
+
+  print(node1);
+}
+
+void push() {
+  final list = LinkedList<int>();
+  list.push(3);
+  list.push(2);
+  list.push(1);
+
+  print(list);
+}
+
+void append() {
+  final list = LinkedList<int>();
+  list.append(1);
+  list.append(2);
+  list.append(3);
+
+  print(list);
+}
+
+void insertAfter() {
+  final list = LinkedList<int>();
+  list.push(3);
+  list.push(2);
+  list.push(1);
+
+  print('Before: $list');
+
+  var middleNode = list.nodeAt(1)!;
+  list.insertAfter(middleNode, 42);
+
+  print('After:  $list');
+}
--- 05-linked-lists/projects/final/bin/linked_list.dart
+++ 05-linked-lists/projects/final/bin/linked_list.dart
@@ -5,4 +5,56 @@ class Node<T> {
   Node({required this.value, this.next});
   T value;
   Node<T>? next;
+
+  @override
+  String toString() {
+    if (next == null) return '$value';
+    return '$value -> ${next.toString()}';
+  }
+}
+
+class LinkedList<E> {
+  Node<E>? head;
+  Node<E>? tail;
+
+  void push(E value) {
+    head = Node(value: value, next: head);
+    tail ??= head;
+  }
+
+  void append(E value) {
+    if (isEmpty) {
+      push(value);
+      return;
+    }
+    tail!.next = Node(value: value);
+    tail = tail!.next;
+  }
+
+  Node<E>? nodeAt(int index) {
+    var currentNode = head;
+    var currentIndex = 0;
+    while (currentNode != null && currentIndex < index) {
+      currentNode = currentNode.next;
+      currentIndex += 1;
+    }
+    return currentNode;
+  }
+
+  Node<E> insertAfter(Node<E> node, E value) {
+    if (tail == node) {
+      append(value);
+      return tail!;
+    }
+    node.next = Node(value: value, next: node.next);
+    return node.next!;
+  }
+
+  bool get isEmpty => head == null;
+
+  @override
+  String toString() {
+    if (isEmpty) return 'Empty list';
+    return head.toString();
+  }
 }
