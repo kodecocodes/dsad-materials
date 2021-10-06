dab19168b213c8c24f673dbc8a6d1acf78ab1284
suragch <suragch@suragch.dev>
2021-10-06 17:44:59 +0800

    working on challenges

--- 05-linked-lists/projects/challenge/bin/challenge.dart
+++ 05-linked-lists/projects/challenge/bin/challenge.dart
@@ -1,4 +1,42 @@
 // Copyright (c) 2021 Razeware LLC
 // For full license & permission details, see LICENSE.
 
-void main() {}
+import 'linked_list.dart';
+
+void main() {
+  challengeOne();
+}
+
+/// Challenge 1: Print in reverse
+///
+/// Create a function that prints the nodes of a linked list in reverse
+/// order. For example:
+///
+/// ```text
+/// 1 -> 2 -> 3 -> null
+///
+/// // should print out the following:
+/// 3
+/// 2
+/// 1
+/// ```
+void challengeOne() {
+  var list = LinkedList<int>();
+  list.push(3);
+  list.push(2);
+  list.push(1);
+
+  print('Original list: $list');
+  print("Printing in reverse:");
+  printListInReverse(list);
+}
+
+void printListInReverse<E>(LinkedList<E> list) {
+  printNodesRecursively(list.head);
+}
+
+void printNodesRecursively<T>(Node<T>? node) {
+  if (node == null) return;
+  printNodesRecursively(node.next);
+  print(node.value);
+}
