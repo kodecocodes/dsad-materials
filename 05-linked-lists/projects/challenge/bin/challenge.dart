dfb79b355db8a5ae5f89397feacdf460e681d62f
suragch <studymongolian@gmail.com>
2021-10-08 17:51:47 +0800

    finished challenges

--- 05-linked-lists/projects/challenge/bin/challenge.dart
+++ 05-linked-lists/projects/challenge/bin/challenge.dart
@@ -4,9 +4,10 @@
 import 'linked_list.dart';
 
 void main() {
-  // challengeOne();
-  // challengeTwo();
+  challengeOne();
+  challengeTwo();
   challengeThree();
+  challengeFour();
 }
 
 /// Challenge 1: Print in reverse
@@ -96,28 +97,94 @@ void challengeThree() {
   print('Reversed list: $list');
 }
 
-extension MyLinkedList<E> on LinkedList<E> {
+extension ReversibleLinkedList<E> on LinkedList<E> {
   // void reverse() {
-  //   final tmpList = LinkedList<E>();
+  //   final tempList = LinkedList<E>();
   //   for (final value in this) {
-  //     tmpList.push(value);
+  //     tempList.push(value);
   //   }
-  //   head = tmpList.head;
+  //   head = tempList.head;
   // }
 
   void reverse() {
     tail = head;
-    var prev = head;
+    var previous = head;
     var current = head?.next;
-    prev?.next = null;
+    previous?.next = null;
 
     while (current != null) {
       final next = current.next;
-      current.next = prev;
-      prev = current;
+      current.next = previous;
+      previous = current;
       current = next;
     }
 
-    head = prev;
+    head = previous;
   }
 }
+
+/// Challenge 4: Remove all occurrences
+///
+/// Create a function that removes all occurrences of a specific element
+/// from a linked list. The implementation is similar to the removeAfter
+/// method that you implemented for the linked list. For example:
+///
+/// ```
+/// original list
+/// 1 -> 3 -> 3 -> 3 -> 4
+///
+/// list after removing all occurrences of 3
+/// 1 -> 4
+/// ```
+void challengeFour() {
+  var list = LinkedList<int>();
+  list.push(3);
+  list.push(2);
+  list.push(2);
+  list.push(1);
+  list.push(1);
+
+  list.removeAll(3);
+  print(list);
+}
+
+extension RemovableLinkedList<E> on LinkedList {
+  void removeAll(E value) {
+    while (head != null && head!.value == value) {
+      head = head!.next;
+    }
+    var previous = head;
+    var current = head?.next;
+    while (current != null) {
+      if (current.value == value) {
+        previous?.next = current.next;
+        current = previous?.next;
+        continue;
+      }
+      previous = current;
+      current = current.next;
+    }
+    tail = previous;
+  }
+}
+
+// extension RemovableLinkedList<E> on LinkedList {
+//   void removeAll(E value) {
+//     while (head != null && head!.value == value) {
+//       head = head!.next;
+//     }
+
+//     var previous = head;
+//     var current = head?.next;
+//     while (current != null) {
+//       if (current.value == value) {
+//         previous?.next = current.next;
+//         current = previous?.next;
+//         continue;
+//       }
+//       previous = current;
+//       current = current.next;
+//     }
+//     tail = previous;
+//   }
+// }
