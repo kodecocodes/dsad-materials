9ba9a357d0848359209aa0a5ad745028456dc18c
suragch <studymongolian@gmail.com>
2021-11-15 19:17:07 +0800

    Chapter 16 working on final materials

--- 16-merge-sort/projects/final/bin/final.dart
+++ 16-merge-sort/projects/final/bin/final.dart
@@ -1,4 +1,11 @@
 // Copyright (c) 2021 Razeware LLC
 // For full license & permission details, see LICENSE.
 
-void main() {}
+import 'package:final_project/merge_sort.dart';
+
+void main() {
+  final list = [7, 2, 6, 3, 9];
+  final sorted = mergeSort(list);
+  print('Original: $list');
+  print('Merge sorted: $sorted');
+}
