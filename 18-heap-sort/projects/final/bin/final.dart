ab7b34f043cb97639bf630381eee2dcae8c301e5
suragch <studymongolian@gmail.com>
2021-11-24 20:08:44 +0800

    Chapter 18 materials done except for tests and challenges

--- 18-heap-sort/projects/final/bin/final.dart
+++ 18-heap-sort/projects/final/bin/final.dart
@@ -1,4 +1,21 @@
 // Copyright (c) 2021 Razeware LLC
 // For full license & permission details, see LICENSE.
 
-void main() {}
+import 'package:final_project/heapsort.dart';
+
+void main() {
+  heapsortUsingHeap();
+  inPlaceHeapsort();
+}
+
+void heapsortUsingHeap() {
+  final sorted = heapsort([6, 12, 2, 26, 8, 18, 21, 9, 5]);
+  print(sorted);
+}
+
+void inPlaceHeapsort() {
+  final list = [6, 12, 2, 26, 8, 18, 21, 9, 5];
+  print(list);
+  list.heapsortInPlace();
+  print(list);
+}
