f354c6fcdb785241dfcc0dbc07d11340175ac979
suragch <studymongolian@gmail.com>
2021-12-11 11:28:11 +0800

    Chapter 22 materials

---
+++ 22-depth-first-search/projects/challenge/lib/depth_first_search.dart
@@ -0,0 +1,34 @@
+// Copyright (c) 2021 Razeware LLC
+// For full license & permission details, see LICENSE.
+
+import 'stack.dart';
+import 'graph.dart';
+
+extension DepthFirstSearch<E> on Graph<E> {
+  List<Vertex<E>> depthFirstSearch(Vertex<E> source) {
+    final stack = Stack<Vertex<E>>();
+    final pushed = <Vertex<E>>{};
+    final visited = <Vertex<E>>[];
+
+    stack.push(source);
+    pushed.add(source);
+    visited.add(source);
+
+    outerLoop:
+    while (stack.isNotEmpty) {
+      final vertex = stack.peek;
+      final neighbors = edges(vertex);
+      for (final edge in neighbors) {
+        if (!pushed.contains(edge.destination)) {
+          stack.push(edge.destination);
+          pushed.add(edge.destination);
+          visited.add(edge.destination);
+          continue outerLoop;
+        }
+      }
+      stack.pop();
+    }
+
+    return visited;
+  }
+}
