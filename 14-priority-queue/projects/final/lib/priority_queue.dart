7a07c2f5697cdaca241a986f4f41ab89d40b4822
suragch <studymongolian@gmail.com>
2022-01-12 19:40:33 +0800

    export Priority from PriorityQueue

--- 14-priority-queue/projects/final/lib/priority_queue.dart
+++ 14-priority-queue/projects/final/lib/priority_queue.dart
@@ -4,6 +4,8 @@
 import 'heap.dart';
 import 'queue.dart';
 
+export 'heap.dart' show Priority;
+
 class PriorityQueue<E extends Comparable<dynamic>> implements Queue<E> {
   PriorityQueue({List<E>? elements, Priority priority = Priority.max}) {
     _heap = Heap<E>(elements: elements, priority: priority);
