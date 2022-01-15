1b7ea96da348a60f0c1c1cba4b5568add52b9096
suragch <studymongolian@gmail.com>
2022-01-15 08:28:12 +0800

    refactor challenge solution to fix bugs

--- 12-binary-search/projects/challenge/bin/challenge.dart
+++ 12-binary-search/projects/challenge/bin/challenge.dart
@@ -45,38 +45,12 @@ void challengeOne() {
   print('Found at index $index');
 }
 
-/// Challenge 2: Searching for a range
-///
-/// Write a function that searches a sorted list and finds the range of
-/// indices for a particular element. You can start by creating a class
-/// named `Range` that holds the start and end indices.
-///
-/// For example:
-///
-/// ```
-/// final list = [1, 2, 3, 3, 3, 4, 5, 5];
-/// final range = findRange(list, value: 3);
-/// ```
-///
-/// `findRange` should return `Range(2, 5)` since those are the start and
-/// end indices for the value `3`.
-void challengeTwo() {
-  // See lib/find_range.dart for implementation details.
-  final list = [1, 2, 3, 3, 3, 4, 5, 5];
-  var range = findRangeUnoptimized(list, 3);
-  print(range);
-
-  range = findRange(list, 3);
-  print(range);
-  // See test/find_range_test.dart for more usage examples.
-}
-
-/// Challenge 3: Non-recursive search
+/// Challenge 2: Non-recursive search
 ///
 /// Does recursion make your brain hurt? No worries, you can always perform
 /// the same task in a non-recursive way. Re-implement `binarySearch` using
 /// a `while` loop.
-void challengeThree() {
+void challengeTwo() {
   int? binarySearch<E extends Comparable<dynamic>>(
     List<E> list,
     E value,
@@ -99,4 +73,30 @@ void challengeThree() {
   final list = [1, 5, 15, 17, 19, 22, 24, 31, 105, 150];
   final index = binarySearch(list, 31);
   print('Found at index $index');
+}
+
+/// Challenge 3: Searching for a range
+///
+/// Write a function that searches a sorted list and finds the range of
+/// indices for a particular element. You can start by creating a class
+/// named `Range` that holds the start and end indices.
+///
+/// For example:
+///
+/// ```
+/// final list = [1, 2, 3, 3, 3, 4, 5, 5];
+/// final range = findRange(list, value: 3);
+/// ```
+///
+/// `findRange` should return `Range(2, 5)` since those are the start and
+/// end indices for the value `3`.
+void challengeThree() {
+  // See lib/find_range.dart for implementation details.
+  final list = [1, 2, 3, 3, 3, 4, 5, 5];
+  var range = findRangeUnoptimized(list, 3);
+  print(range);
+
+  range = findRange(list, 3);
+  print(range);
+  // See test/find_range_test.dart for more usage examples.
 }
--- 12-binary-search/projects/challenge/lib/find_range.dart
+++ 12-binary-search/projects/challenge/lib/find_range.dart
@@ -10,70 +10,51 @@ class Range {
   String toString() => 'Range($start, $end)';
 }
 
-Range? findRangeUnoptimized<E>(List<E> list, E value) {
+Range? findRangeUnoptimized(List<int> list, int value) {
   final start = list.indexOf(value);
   if (start == -1) return null;
   final end = list.lastIndexOf(value) + 1;
   return Range(start, end);
 }
 
-Range? findRange<E extends Comparable<dynamic>>(
-  List<E> list,
-  E value,
-) {
+Range? findRange(List<int> list, int value) {
   if (list.isEmpty) return null;
-  final start = _startIndex(list, value, Range(0, list.length));
-  if (start == null) return null;
-  final end = _endIndex(list, value, Range(0, list.length));
-  return Range(start, end!);
+  final start = _startIndex(list, value);
+  final end = _endIndex(list, value);
+  if (start == null || end == null) return null;
+  return Range(start, end);
 }
 
-int? _startIndex<E extends Comparable<dynamic>>(
-  List<E> list,
-  E value,
-  Range range,
-) {
-  final size = range.end - range.start;
-  final middle = range.start + size ~/ 2;
-
-  if (middle == 0 || middle == list.length - 1) {
-    if (list[middle] == value) {
+int? _startIndex(List<int> list, int value) {
+  if (list[0] == value) return 0;
+  var start = 1;
+  var end = list.length;
+  while (start < end) {
+    var middle = start + (end - start) ~/ 2;
+    if (list[middle] == value && list[middle - 1] != value) {
       return middle;
+    } else if (list[middle] < value) {
+      start = middle + 1;
     } else {
-      return null;
+      end = middle;
     }
   }
-
-  if (list[middle] == value && list[middle - 1] != value) {
-    return middle;
-  } else if (value.compareTo(list[middle]) > 0) {
-    return _startIndex(list, value, Range(middle, range.end));
-  } else {
-    return _startIndex(list, value, Range(range.start, middle));
-  }
+  return null;
 }
 
-int? _endIndex<E extends Comparable<dynamic>>(
-  List<E> list,
-  E value,
-  Range range,
-) {
-  final size = range.end - range.start;
-  final middle = range.start + size ~/ 2;
-
-  if (middle == 0 || middle == list.length - 1) {
-    if (list[middle] == value) {
+int? _endIndex(List<int> list, int value) {
+  if (list[list.length - 1] == value) return list.length;
+  var start = 0;
+  var end = list.length - 1;
+  while (start < end) {
+    var middle = start + (end - start) ~/ 2;
+    if (list[middle] == value && list[middle + 1] != value) {
       return middle + 1;
+    } else if (list[middle] > value) {
+      end = middle;
     } else {
-      return null;
+      start = middle + 1;
     }
   }
-
-  if (list[middle] == value && list[middle + 1] != value) {
-    return middle + 1;
-  } else if (value.compareTo(list[middle]) < 0) {
-    return _endIndex(list, value, Range(range.start, middle));
-  } else {
-    return _endIndex(list, value, Range(middle, range.end));
-  }
+  return null;
 }
